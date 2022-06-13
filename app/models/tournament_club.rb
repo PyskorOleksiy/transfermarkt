1bc82cd4657c10f43853fe5bf2cbc0eb20d6c78b
PyskorOleksiy <76560253+PyskorOleksiy@users.noreply.github.com>
2022-06-13 20:07:14 +0300

    Delete tournament_club.rb

--- app/models/tournament_club.rb
+++
@@ -1,6 +0,0 @@
-class TournamentClub < ApplicationRecord
-  has_many :homes, dependent: :destroy
-	has_many :aways, dependent: :destroy
-  has_many :players, dependent: :destroy
-  has_one :coach, dependent: :destroy
-end
